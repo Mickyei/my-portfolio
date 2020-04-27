cc4cdd3108737f85bbcff1c875bc8533d44920f1
Mickyei <micky.kyei@cs.tamk.fi>
2020-04-27 17:33:28 +0300

    Updates

---
+++ precache-manifest.30a70e6cd9b62fc484e548f3afe83990.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9a14837a41d57260a3632ff7908b45",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "ac9f4176b0bdc707f5b2",
+    "url": "/my-portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1fa7fb7d9f508740bd81",
+    "url": "/my-portfolio/static/css/main.054f5d27.chunk.css"
+  },
+  {
+    "revision": "ac9f4176b0bdc707f5b2",
+    "url": "/my-portfolio/static/js/2.874e5637.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-portfolio/static/js/2.874e5637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa7fb7d9f508740bd81",
+    "url": "/my-portfolio/static/js/main.4e31d7bc.chunk.js"
+  },
+  {
+    "revision": "6d7a7337562760299d40",
+    "url": "/my-portfolio/static/js/runtime-main.00bba678.js"
+  },
+  {
+    "revision": "32d04fa75bd4856a184afaa0fc8a276b",
+    "url": "/my-portfolio/static/media/athlete2.32d04fa7.jpg"
+  },
+  {
+    "revision": "62728c6c1ac318fa3cf034b9dc7d5e75",
+    "url": "/my-portfolio/static/media/micky_circle.62728c6c.png"
+  },
+  {
+    "revision": "bc20c0732dc4eaceb98238c10dbfc230",
+    "url": "/my-portfolio/static/media/tamk.bc20c073.jpg"
+  }
+]);
